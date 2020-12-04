1c67d39d451fda2533519ecb6c99b798d885c3df
Prathamesh Raut <prathamesh.raut@neoquant.com>
2020-12-04 16:58:41 +0530

    AuthenticationEntryPoint Filter added for 403 case - AuthenticationEntryPoint child class Http403AuthenticationEntryPoint

---
+++ src/main/java/com/dev/neo/supportportal/filter/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,39 @@
+package com.dev.neo.supportportal.filter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+
+import  com.dev.neo.supportportal.domain.ServiceResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static com.dev.neo.supportportal.constant.SecurityConstant.*;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint
+{
+	/**
+	 * Always returns a 403 error code to the client on Unauthorized Access
+	 */
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+	      throws IOException
+	{
+		ServiceResponse serviceResponse = new ServiceResponse(FORBIDDEN.value(), FORBIDDEN, FORBIDDEN.getReasonPhrase().toUpperCase(), FORBIDDEN_MESSAGE );
+		response.setContentType(APPLICATION_JSON_VALUE);
+		response.setStatus(FORBIDDEN.value());
+		OutputStream outputStream = response.getOutputStream();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.writeValue(outputStream, serviceResponse);
+		outputStream.flush();
+	}
+}
