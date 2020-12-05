a61f343fbf826e9959c65ade3be6a1a3cf0d3972
Prathamesh Raut <prathamesh.raut@neoquant.com>
2020-12-05 21:40:14 +0530

    manage imports

--- src/main/java/com/dev/neo/supportportal/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/dev/neo/supportportal/filter/JwtAuthorizationFilter.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.dev.neo.supportportal.constant.SecurityConstant;
