60d191cc7fb83f2f0dbeca66b5c07477f6d6be06
Prathamesh Raut <prathamesh.raut@neoquant.com>
2020-12-09 16:36:52 +0530

    Extends with global Exception handler class

--- src/main/java/com/dev/neo/supportportal/controller/UserController.java
+++ src/main/java/com/dev/neo/supportportal/controller/UserController.java
@@ -4,13 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dev.neo.supportportal.exception.domain.ExceptionHandling;
+import com.dev.neo.supportportal.exception.domain.UserNotFoundException;
+
 @RestController
-@RequestMapping(value="/user")
-public class UserController
+@RequestMapping(value = "/user")
+public class UserController extends ExceptionHandling
 {
 
 	@GetMapping(value = "/home")
-	public String showUser() {
-		return "Welcome ...";
+	public String showUser() throws UserNotFoundException{
+//		return "Welcome ...";
+		throw new UserNotFoundException("User does not exist");
 	}
 }
