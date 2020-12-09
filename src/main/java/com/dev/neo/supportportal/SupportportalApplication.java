6e7240626f537ec45d8722456f3e06d10ac00c77
Prathamesh Raut <prathamesh.raut@neoquant.com>
2020-12-09 17:50:09 +0530

    EnableAutoConfig Annotation commented for reference

--- src/main/java/com/dev/neo/supportportal/SupportportalApplication.java
+++ src/main/java/com/dev/neo/supportportal/SupportportalApplication.java
@@ -1,11 +1,14 @@
 package com.dev.neo.supportportal;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class}) /*Removes default whitelabel  spring error page*/
 public class SupportportalApplication {
 
 	public static void main(String[] args) {
