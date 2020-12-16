7047a2109536738b7573ac38f92c36c77b51e7b0
Prathamesh Raut <prathamesh.raut@neoquant.com>
2020-12-16 11:46:06 +0530

    user servie method added

--- src/main/java/com/dev/neo/supportportal/service/UserService.java
+++ src/main/java/com/dev/neo/supportportal/service/UserService.java
@@ -1,6 +1,19 @@
 package com.dev.neo.supportportal.service;
 
+import java.util.List;
+
+import com.dev.neo.supportportal.domain.User;
+import com.dev.neo.supportportal.exception.domain.EmailExistException;
+import com.dev.neo.supportportal.exception.domain.UserNotFoundException;
+import com.dev.neo.supportportal.exception.domain.UsernameExistException;
+
 public interface UserService
 {
-
+	User register(String firstName, String lastName, String userName, String email) throws UserNotFoundException, UsernameExistException, EmailExistException;
+	
+	List<User> getUsers();
+	
+	User findByUserName(String userName);
+	
+	User findUserByEmail(String email);
 }
