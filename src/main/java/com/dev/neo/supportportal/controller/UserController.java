f064184971abeb5963644276db1fe1c29005160f
Prathamesh Raut <prathamesh.raut@neoquant.com>
2020-12-16 11:45:05 +0530

    register mapping added

--- src/main/java/com/dev/neo/supportportal/controller/UserController.java
+++ src/main/java/com/dev/neo/supportportal/controller/UserController.java
@@ -1,20 +1,31 @@
 package com.dev.neo.supportportal.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dev.neo.supportportal.domain.User;
+import com.dev.neo.supportportal.exception.domain.EmailExistException;
 import com.dev.neo.supportportal.exception.domain.ExceptionHandling;
 import com.dev.neo.supportportal.exception.domain.UserNotFoundException;
+import com.dev.neo.supportportal.exception.domain.UsernameExistException;
+import com.dev.neo.supportportal.service.UserService;
 
 @RestController
 @RequestMapping(path = { "/", "/user" })
 public class UserController extends ExceptionHandling
 {
 
-	@GetMapping(value = "/home")
-	public String showUser() throws UserNotFoundException{
-//		return "Welcome ...";
-		throw new UserNotFoundException("User does not exist");
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping(value = "/register")
+	public ResponseEntity<User> registerUser(@RequestBody User user) throws UserNotFoundException, UsernameExistException, EmailExistException{
+		User newUser = userService.register(user.getFirstName(), user.getLastName(), user.getUserName(), user.getEmail());
+		return new ResponseEntity<>(newUser, HttpStatus.OK);
 	}
 }
