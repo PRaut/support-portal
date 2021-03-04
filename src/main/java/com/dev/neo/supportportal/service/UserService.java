f32ce902c28cae8b39f8d661411f5909d2ea045c
prathamesh.raut <prathamesh.raut@gmail.com>
2021-03-04 11:51:25 +0530

    CRUD method added for User

--- src/main/java/com/dev/neo/supportportal/service/UserService.java
+++ src/main/java/com/dev/neo/supportportal/service/UserService.java
@@ -1,9 +1,13 @@
 package com.dev.neo.supportportal.service;
 
+import java.io.IOException;
 import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import com.dev.neo.supportportal.domain.User;
 import com.dev.neo.supportportal.exception.domain.EmailExistException;
+import com.dev.neo.supportportal.exception.domain.EmailNotFoundException;
 import com.dev.neo.supportportal.exception.domain.UserNotFoundException;
 import com.dev.neo.supportportal.exception.domain.UsernameExistException;
 
@@ -16,4 +20,14 @@ public interface UserService
 	User findByUserName(String userName);
 	
 	User findUserByEmail(String email);
+	
+	User addUser(String firstName, String lastName, String username, String email, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException;
+	
+	User updateUser(String currentUsername, String newFirstname, String newLastname, String newUsername, String newEmail, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException;
+	
+	void deleteUser(Long id);
+	
+	void resetPassword(String email) throws EmailNotFoundException;
+	
+	User updateProfileImage(String username, MultipartFile profileFile) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException;
 }
