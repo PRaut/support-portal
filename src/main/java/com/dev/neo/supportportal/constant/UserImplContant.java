d92d9e4cadf98219bb0aa6ca588b5c6b9e83545f
prathamesh.raut <prathamesh.raut@gmail.com>
2021-03-04 11:49:58 +0530

    new no_user_found_by_email constant added

--- src/main/java/com/dev/neo/supportportal/constant/UserImplContant.java
+++ src/main/java/com/dev/neo/supportportal/constant/UserImplContant.java
@@ -2,9 +2,10 @@ package com.dev.neo.supportportal.constant;
 
 public class UserImplContant
 {
-	public static final String DEFAULT_USER_PROFILE_IMAGE_PATH = "/user/image/profile/temp";
+
 	public static final String FOUND_USER_WITH_USERNAME = "Returning found user with username: ";
 	public static final String NO_USER_FOUND_BY_USERNAME = "No user found by username ";
 	public static final String EMAIL_ALREADY_EXISTS = "Email already exists";
 	public static final String USERNAME_ALREADY_EXISTS = "Username already exists";
+	public static final String NO_USER_FOUND_BY_EMAIL = "No email found for email: ";
 }
