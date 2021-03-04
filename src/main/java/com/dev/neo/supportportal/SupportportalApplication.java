c405663bd8715cf7c9d60d61ee35a82eabc897fe
prathamesh.raut <prathamesh.raut@gmail.com>
2021-03-04 11:48:13 +0530

    directory created to store app documents

--- src/main/java/com/dev/neo/supportportal/SupportportalApplication.java
+++ src/main/java/com/dev/neo/supportportal/SupportportalApplication.java
@@ -1,18 +1,21 @@
 package com.dev.neo.supportportal;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import static com.dev.neo.supportportal.constant.FileConstant.*;
+
+import java.io.File;
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class}) /*Removes default whitelabel  spring error page*/
 public class SupportportalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupportportalApplication.class, args);
+		System.out.println(USER_FOLDER);
+		new File(USER_FOLDER).mkdirs();  
 	}
 
 	@Bean
