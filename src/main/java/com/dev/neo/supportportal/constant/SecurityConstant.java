62a5a50731d31cf5dc3962f5bbe345b6125a6525
prathamesh.raut <prathamesh.raut@gmail.com>
2021-03-04 11:49:25 +0530

    jwt token header updated to Bearer and public links updated

--- src/main/java/com/dev/neo/supportportal/constant/SecurityConstant.java
+++ src/main/java/com/dev/neo/supportportal/constant/SecurityConstant.java
@@ -4,7 +4,8 @@ public class SecurityConstant
 {
 	public static final long EXPIRATION_TIME = 432_000_000; // 5 day expressed in miliseconds
 	public static final String TOKEN_PREFIX = "Bearer ";
-	public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+//	public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+	public static final String JWT_TOKEN_HEADER = "Bearer ";
 	public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
 	public static final String NEOQUANT_SOLUTIONS = "Neoquant Solutions Pvt Ltd";
 	public static final String NEOQUANT_ADMINISTRATION = "User Management Portal";
@@ -13,4 +14,5 @@ public class SecurityConstant
 	public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
 	public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
 	public static final String[] PUBLIC_URLS = {"/user/login", "/user/register", "/user/resetPassword/**", "/user/image/**"};
+//	public static final String[] PUBLIC_URLS = {"**"};
 }
